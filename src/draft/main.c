28eff2c39c96384a4a5709ef81a321513d1902dd
Aline Bessa <alibezz@gmail.com>
2011-05-27 20:25:12 -0300

    Now, content chosen from last combobox is appended to textbox buffer

--- src/draft/main.c
+++ src/draft/main.c
@@ -1,8 +1,24 @@
 #include "formulae_textbox_widget.h"
+#include <string.h>
+#define BUFFER_MAX_LENGTH 3000
+
+static void cb_changed( GtkComboBoxText *combo, GtkEntryBuffer *buffer) {
+    gchar *string = gtk_combo_box_text_get_active_text( combo );
+ 
+    /* Prints it to the console - if nothing is selected, print NULL */
+    g_print( "Selected (simple): >> %s <<\n", ( string ? string : "NULL" ) );
+    
+   /* Appends string to buffer */
+   gtk_entry_buffer_insert_text(buffer, gtk_entry_buffer_get_length(buffer), string, -1);
+    /* Frees string */
+    g_free( string );
+}
+
 
 int main (int argc, char *argv[]) {
 
   GtkWidget *window, *table, *entry, *grid, *combo, *frame;
+  GtkEntryBuffer *buffer;
 
   gtk_init(&argc, &argv);
 
@@ -13,7 +29,11 @@ int main (int argc, char *argv[]) {
 
   /* Creates a 1x1 table and attaches a textbox on it */
   table = gtk_table_new(1, 1, TRUE);
-  entry = gtk_entry_new();
+ 
+  buffer = gtk_entry_buffer_new(NULL, 0); 
+  entry = gtk_entry_new_with_buffer(buffer);
+  gtk_entry_buffer_set_max_length(buffer, BUFFER_MAX_LENGTH);
+  
   gtk_entry_set_max_length(GTK_ENTRY (entry),0);
   gtk_table_attach_defaults(GTK_TABLE (table),entry, 0, 1, 0, 1); 
 
@@ -70,6 +90,9 @@ int main (int argc, char *argv[]) {
   /* And adds new frame to grid */
   gtk_grid_attach(GTK_GRID(grid), frame, 2, 1, 1, 1);
 
+  /* Connects a handler to last combobox */
+  g_signal_connect( G_OBJECT( combo ), "changed", G_CALLBACK( cb_changed ), GTK_ENTRY_BUFFER(buffer));
+ 
 
   /* Shows all widgets recursively */
   gtk_widget_show_all(window); 
